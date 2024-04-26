faa22f2073da4d92861d850e051ffefaa9f9bf7f
MrFiruma <100999609+MrFiruma@users.noreply.github.com>
2024-04-26 16:47:58 +0200

    Update scan_asynch.py
    Ajout d'une formule "wait" pour s'assurer que la connexion se termine avant de renvoyer un résultat au main

--- testport/modules/scan_asynch.py
+++ testport/modules/scan_asynch.py
@@ -8,7 +8,8 @@ class ScanASync:
         try:
             # Ouvre une connexion asynchrone au port donné
             lecture, ecriture = await asyncio.open_connection(self.host, port)
-            ecriture.close()  # Ferme la connexion
-            return port, True  # Si la connexion réussit, le port est ouvert
+            ecriture.close()
+            await ecriture.wait_closed()  # /!\ si c'est pas fermé ça crée des erreurs 
+            return port, True 
         except (asyncio.TimeoutError, ConnectionRefusedError):
-            return port, False  # Si la connexion échoue, le port est fermé
+            return port, False
