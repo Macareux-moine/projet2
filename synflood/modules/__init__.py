28fe2b5cc96bec8b98ea0bab0da38ec6fee4186c
Macareux-moine <fakirefak@gmail.com>
2024-04-26 18:19:08 +0200

    ne marche pas dans importation et j'ai eu la flemme de cherche pouquoi donc j'ai crée demmande_user a ala place

--- synflood/modules/__init__.py
+++ synflood/modules/__init__.py
@@ -1,2 +1,14 @@
-class instruction
-    input
+from modules.synflood_multi import ENVOIS
+
+def main():
+    # Demander à l'utilisateur les informations nécessaires
+    cible = input("Adresse cible : ")
+    port = int(input("Port : "))
+    nbrpaquet = int(input("Nombre de paquets : "))
+    nbr_processus = int(input("Nombre de processus : "))
+
+    # Appeler la fonction syn de la classe ENVOIS avec les paramètres fournis par l'utilisateur
+    ENVOIS.syn(cible, port, nbrpaquet, nbr_processus)
+
+if __name__ == "__main__":
+    main()
