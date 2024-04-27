ceb57e6f5f7248a0603d4831551500856e9a20d8
Macareux-moine <fakirefak@gmail.com>
2024-04-27 21:26:21 +0200

    renomage de demande_user

---
+++ synflood/modules/demande_user.py
@@ -0,0 +1,14 @@
+class DemandeUtilisateur:
+    def obtenir_informations(self):
+        # Demander à l'utilisateur les informations nécessaires
+        cible = input("Adresse cible : ")
+        port = int(input("Port : "))#obliger avoir un int(input) pour avoir un entier sinon bloucle infini entre synflod_multi et demande_user
+        nbrpaquet = int(input("Nombre de paquets : "))
+        nbr_processus = int(input("Nombre de processus : "))
+        return cible, port, nbrpaquet, nbr_processus
+
+if __name__ == "__main__":
+    demande_utilisateur = DemandeUtilisateur()
+    cible, port, nbrpaquet, nbr_processus = demande_utilisateur.obtenir_informations()
+    print(f"Cible : {cible}, Port : {port}, Nombre de paquets : {nbrpaquet}, Nombre de processus : {nbr_processus}")
+
