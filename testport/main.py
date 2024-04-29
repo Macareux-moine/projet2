c3fe9293ac01e5967cbd9a1d384df8a751454e93
MrFiruma <100999609+MrFiruma@users.noreply.github.com>
2024-04-29 12:23:53 +0200

    Update main.py
    Modification de la methode asynchrone pour qu'elle soit vraiment asynchrone
    
    - ajout de la methode thread (non focntionnel)

--- testport/main.py
+++ testport/main.py
@@ -1,50 +1,73 @@
 import asyncio
 from modules.scan_synch import ScanSynch
 from modules.scan_asynch import ScanASync
+from modules.scan_thread import ScanThread
 
 async def main():
     hote = input('Hote (localhost) : ')
-    mode_scan = input('Mode de scan (synch/asynch) : ').lower()
-    port_debut = int(input('Port de départ : '))
-    port_fin = int(input('Port de fin : '))
+    mode_scan = input('Mode de scan (synch/asynch/Thread) : ').lower()
+    start_port = int(input('Port de départ : '))
+    end_port = int(input('Port de fin : '))
     
     # Crée une liste de ports à scanner
-    ports = range(port_debut, port_fin + 1)
+    ports = range(start_port, end_port + 1)
     
-    # Execution du scan en mode synchrone
-    if mode_scan == 'synch':
-        scann = ScanSynch(hote)
-        port_ouvert = []
-        # Ajout des ports dans la liste
-        for port in ports:
-            resultat = scann.scan(port)
-            print('test du port %s' % port)
-            # Verifier si le port est ouvert pour l'ajouter au tableau
-            if resultat is not None and resultat[1]:
-                port_ouvert.append(resultat[0])
+    try:
+        
+        # Execution du scan en mode synchrone
+        if mode_scan == 'synch':
+            scann = ScanSynch(hote)
+            port_ouvert = []
+            # Ajout des ports dans la liste
+            for port in ports:
+                resultat = scann.scan(port)
+                print('test du port %s' % port)
+                # Verifier si le port est ouvert pour l'ajouter au tableau
+                if resultat is not None and resultat[1]:
+                    port_ouvert.append(resultat[0])
 
-        print('Scan terminé')
-        for port in port_ouvert:
-            print('Port ouvert :', port)
-    
-    # Execution du scan en mode asynchrone
-    elif mode_scan == 'asynch':
-        scann = ScanASync(hote)
+            print('Scan terminé')
+            for port in port_ouvert:
+                print('Port ouvert :', port)
+        
+        # Execution du scan en mode asynchrone
+        elif mode_scan == 'asynch':
+            scann = ScanASync(hote)
 
-        # Exécute le scan de manière asynchrone pour chaque port
-        port_ouvert = []
-        for port in ports:
-            resultat = await scann.scan(port)
-            print('test du port %s' % port)
-            # Verifier si le port est ouvert pour l'ajouter au tableau
-            if resultat is not None and resultat[1]:
-                port_ouvert.append(resultat[0])
+            # Liste pour stocker les futures de chaque scan asynchrone
+            port_ouvert = []
 
-        print('Scan terminé')
-        for port in port_ouvert:
-            print('Port ouvert :', port)
-    else:
-        print('Mode de scan invalide.')
+            # Lancer les scans asynchrones pour chaque port
+            for port in ports:
+                print('test du port %s' % port)
+                port_ouvert.append(scann.scan(port))
+
+            # Attendre que tous les scans asynchrones soient terminés
+            resultat = await asyncio.gather(*port_ouvert)
+
+            # Afficher les résultats dans l'ordre
+            print('Scan terminé')
+            for port, status in resultat:
+                if status:
+                    print('Port ouvert :', port)
+                else:
+                    print('Port fermé :', port)
+
+                
+        elif mode_scan == 'thread':
+            scann = ScanThread(hote)
+            port_ouvert = []
+            for port in ports:
+                resultat = scann.scan(port)
+                print('test du port %s' % port)
+                # Verifier si le port est ouvert pour l'ajouter au tableau
+                if resultat is not None and resultat[1]:
+                    port_ouvert.append(resultat[0])        
+            
+        else:
+            print('Mode de scan invalide.')
+    except Exception as ex:
+        print('Erreur %s dans la focntion main()' % ex)
 
 if __name__ == '__main__':
     asyncio.run(main())
