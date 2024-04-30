44452aa936c56c84b6c324e9111a7a7e0d57579e
MrFiruma <100999609+MrFiruma@users.noreply.github.com>
2024-04-30 14:49:13 +0200

    Update scan_asynch.py
    Ajout de commentaires

--- testport/modules/scan_asynch.py
+++ testport/modules/scan_asynch.py
@@ -1,15 +1,14 @@
 import asyncio
 
-class ScanASync:
+class ScanASync:  # Définition d'une classe ScanASync pour effectuer des scans de ports asynchrones
     def __init__(self, host):
         self.host = host
 
-    async def scan(self, port):
+    async def scan(self, port):  # Méthode asynchrone pour scanner un port donné
         try:
-            # Ouvre une connexion asynchrone au port donné
-            lecture, ecriture = await asyncio.open_connection(self.host, port)
-            ecriture.close()
-            await ecriture.wait_closed()  # /!\ si c'est pas fermé ça crée des erreurs 
-            return port, True 
-        except (asyncio.TimeoutError, ConnectionRefusedError):
-            return port, False
+            lecture, ecriture = await asyncio.open_connection(self.host, port) # Ouvre une connexion asynchrone au port donné
+            ecriture.close()  # Fermeture du canal d'écriture
+            await ecriture.wait_closed()  # Attente de la fermeture complète de la connexion /!\ crée des errurs si ce n'est pas entierement fermé
+            return port, True  # Si la connexion réussit, retourner le numéro de port et True
+        except (asyncio.TimeoutError, ConnectionRefusedError):  # Gestion des exceptions pour les erreurs de connexion
+            return port, False  # Si la connexion échoue, retourner le numéro de port et False
