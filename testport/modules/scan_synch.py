ee5a28fef4dba1ba2833d5b33fec7a7256efc11c
MrFiruma <100999609+MrFiruma@users.noreply.github.com>
2024-04-30 14:49:52 +0200

    Update scan_synch.py
    Ajout de commentaires

--- testport/modules/scan_synch.py
+++ testport/modules/scan_synch.py
@@ -1,15 +1,15 @@
 import socket
 
-class ScanSynch:
+class ScanSynch:  # Définition d'une classe ScanSynch pour effectuer des scans de ports synchrones
     def __init__(self, hote):
-        self.hote = hote
+        self.hote = hote 
 
-    # Creation d'une socket et tentative de connection au port donné
+    # Méthode pour scanner un port donné
     def scan(self, port):
         try:
-            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:  # Création d'une socket TCP
                 s.settimeout(1)
-                s.connect((self.hote, port))
-                return port, True
-        except (socket.error, socket.timeout, ConnectionRefusedError):
-            return port, False
+                s.connect((self.hote, port))  # Tentative de connexion au port spécifié
+                return port, True  # Si la connexion est réussie, retourner le port et True
+        except (socket.error, socket.timeout, ConnectionRefusedError):  # Gestion des exceptions pour les erreurs de connexion
+            return port, False  # Si la connexion échoue, retourner le port et False
